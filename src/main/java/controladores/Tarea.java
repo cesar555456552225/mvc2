131fa6faf493547c0cd1ed98b05f881144982272
Personal <Personal@SOGAPRCMRFSD408.FormacionSena.RED>
2025-04-25 09:18:41 -0500

    se crea una carpeta para los controladores

---
+++ src/main/java/controladores/Tarea.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controladores;
+
+/**
+ *
+ * @author Personal
+ */
+public class Tarea {
+    private int id;
+    private String descripcion;
+    private boolean completada;
+    
+    public Tarea(int id, String descripcion){
+        this.id=id;
+        this.descripcion=descripcion;
+        this.completada=false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+}
